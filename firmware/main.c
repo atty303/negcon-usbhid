cb3ba3b0f92e8da5e6a132a128b19c0e032cec6d
AGAWA Koji <atty303@gmail.com>
2011-05-02 23:21:30 +0900

    Import V-USB sources.

--- firmware/main.c
+++ firmware/main.c
@@ -1,3 +1,4 @@
+/* -*- c-file-style: "stroustrup"; -*- */
 /* Name: main.c
  * Author: <insert your name here>
  * Copyright: <insert your copyright message here>
@@ -5,6 +6,12 @@
  */
 
 #include <avr/io.h>
+#include "usbdrv.h"
+
+usbMsgLen_t usbFunctionSetup(uchar setupData[8])
+{
+    return 0;
+}
 
 int main(void)
 {
